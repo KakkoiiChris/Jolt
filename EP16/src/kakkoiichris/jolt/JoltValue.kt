81f6966de647f9458dfe3f415d7da6e5fdb6066f
Christian Alexander <kakkoiichris@gmail.com>
2025-09-02 01:42:45 -0400

    Finished Episode 16.

---
+++ EP16/src/kakkoiichris/jolt/JoltValue.kt
@@ -0,0 +1,80 @@
+package kakkoiichris.jolt
+
+import kakkoiichris.jolt.parser.Stmt
+import kakkoiichris.jolt.runtime.Memory
+import kotlin.math.floor
+
+interface JoltValue<X> {
+    val value: X
+
+    val type: String
+
+    val iterable: List<JoltValue<*>> get() = listOf(this)
+
+    companion object {
+        fun of(x: Any) = when (x) {
+            Unit       -> JoltUnit
+
+            is Boolean -> JoltBool(x)
+
+            is Double  -> JoltNum(x)
+
+            is String  -> JoltString(x)
+
+            else       -> TODO("NOT A VALUE")
+        }
+    }
+}
+
+data object JoltUnit : JoltValue<Unit> {
+    override val value = Unit
+
+    override val type = "unit"
+}
+
+data class JoltBool(override val value: Boolean) : JoltValue<Boolean> {
+    override val type = "bool"
+
+    override fun toString() = value.toString()
+}
+
+data class JoltNum(override val value: Double) : JoltValue<Double> {
+    override val type = "num"
+
+    override val iterable get() = (0..<value.toInt()).map { JoltNum(it.toDouble()) }.toList()
+
+    private fun truncate() = (
+            if (value == floor(value))
+                value.toInt()
+            else
+                value
+                             ).toString()
+
+    override fun toString() = truncate()
+}
+
+data class JoltString(override val value: String) : JoltValue<String> {
+    override val type = "string"
+
+    override val iterable get() = value.toCharArray().map { JoltString(it.toString()) }.toList()
+
+    override fun toString() = "\"$value\""
+}
+
+data class JoltList(override val value: MutableList<JoltValue<*>>) : JoltValue<MutableList<JoltValue<*>>> {
+    override val type get() = "list"
+
+    override val iterable get() = value.toList()
+
+    override fun toString() = if (value.isNotEmpty())
+        value.joinToString(prefix = "[", postfix = "]", separator = ", ") { it.toString() }
+    else
+        "[]"
+}
+
+data class JoltFun(override val value: Stmt.Fun, val scope: Memory.Scope) : JoltValue<Stmt.Fun> {
+    override val type get() = "fun"
+
+    override fun toString() =
+        "fun ${value.name}${value.params.joinToString(prefix = "(", postfix = ")", separator = ", ") { it.name.value }}"
+}
